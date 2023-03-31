f31bf9c52766129b641a16238392a2b7c81f868c
melody-story <rnlqls7454@naver.com>
2023-03-31 10:16:26 +0900

    HttpRequest data - POST message body(JSON)

---
+++ servelt/src/main/java/hello/servelt/basic/request/HelloData.java
@@ -0,0 +1,11 @@
+package hello.servelt.basic.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+    private String username;
+    private int age;
+}
